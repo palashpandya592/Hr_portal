816db63cd9115deed162f701a90a96a28cd0c634
pranitha <sandupatla.pranitha@gmail.com>
2022-05-23 10:36:38 +0530

    added login Screen Ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,10 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:interviewapp/firebase/authentication_service.dart';
-import 'package:interviewapp/interviewListPage/ListScreen.dart';
-import 'package:interviewapp/interviewListPage/SignInPage.dart';
+import 'package:interviewapp/interviewListPage/LoginPage.dart';
 import 'package:provider/provider.dart';
 
-Future<void> main() async{
-  WidgetsFlutterBinding.ensureInitialized();
- await Firebase.initializeApp();
+void main() {
   runApp(const MyApp());
 }
 
@@ -16,175 +12,46 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        Provider<AuthenticationService>(
-          create:(_) => AuthenticationService(FirebaseAuth.instance),
-        ),
-        StreamProvider(
-          create: (context) => context.read<AuthenticationService>().authStateChanges,
-          initialData: null,)
-      ],
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: const AuthenticationPage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class AuthenticationPage extends StatefulWidget {
+  const AuthenticationPage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<AuthenticationPage> createState() => _AuthenticationPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _AuthenticationPageState extends State<AuthenticationPage> {
 
-  TextEditingController _username = new TextEditingController();
-  TextEditingController _password = new TextEditingController();
+  Future<FirebaseApp> _initializeFirebase() async{
+    FirebaseApp firebaseApp =await Firebase.initializeApp();
+    return firebaseApp;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topRight,
-              end: Alignment.bottomLeft,
-              colors: [
-                Colors.lightBlueAccent.withOpacity(0.7),
-                Colors.purpleAccent.withOpacity(0.5),
-              ],
-            )),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                height: 60,
-                width: 60,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(35),
-                  border: Border.all(color: Colors.white,width: 1.5)
-                ),
-                child: Icon(
-                  Icons.person,
-                  color: Colors.white,
-                  size: 50,
-                ),
-              ),
-              const SizedBox(height: 70),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: Colors.transparent,
-                      borderRadius: BorderRadius.circular(10),
-                      border: Border.all(width: 1, color: Colors.black)),
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 8.0),
-                    child: TextField(
-                      style: Theme.of(context).textTheme.bodyText2,
-                      maxLines: 1,
-                      controller: _username,
-                      textAlign: TextAlign.left,
-                      decoration: const InputDecoration(
-                          border: InputBorder.none,
-                          labelText:"Email",
-                          icon:  Icon(
-                            Icons.person,
-                            color: Colors.blue,
-                          )),
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: Colors.transparent,
-                      borderRadius: BorderRadius.circular(10),
-                      border: Border.all(width: 1, color: Colors.black)),
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 8.0),
-                    child: TextField(
-                      style: Theme.of(context).textTheme.bodyText2,
-                      maxLines: 1,
-                      obscureText: true,
-                      controller: _password,
-                      textAlign: TextAlign.left,
-                      decoration:  const InputDecoration(
-                          border: InputBorder.none,
-                          labelText:"Password",
-                          icon:  Icon(
-                            Icons.lock,
-                            color: Colors.blue,
-                          )),
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 20),
-              SizedBox(
-                width:  150,
-                height:  35,
-                child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.blue,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                    ),
-                    child: Wrap(
-                      crossAxisAlignment: WrapCrossAlignment.center,
-                      alignment: WrapAlignment.center,
-                      children: const [
-                        Text("Login",style: TextStyle(fontWeight: FontWeight.w500)),
-                      ],
-                    ),
-                    onPressed:(){
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => listScreen()));
-                    }),
-              ),
-              const SizedBox(height: 20),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("Don't have a Account?"),
-                  TextButton(
-                      child: Text('SignUp'),
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => signInPage()));
-                      }
-                  )
-                ],
-              ),
-            ],
-          ),
-        ),
+      body: FutureBuilder(
+        future: _initializeFirebase(),
+        builder: (context,snapshot) {
+         if(snapshot.connectionState == ConnectionState.done){
+           return const LoginPage();
+         }
+         return const Center(
+           child: CircularProgressIndicator(),
+         );
+        }
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
